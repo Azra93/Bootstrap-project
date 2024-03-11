be076deafd50d50614023babca272eecd26281e0
Azra <104520392+Azra93@users.noreply.github.com>
2024-03-11 21:33:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,411 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- CSS file -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Oswald:wght@300&family=Rubik&display=swap" rel="stylesheet">
+    <!-- Font AWesome -->
+    <script src="https://kit.fontawesome.com/c26cd2166c.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg ">
+  <div class="container">
+    <a class="navbar-brand" href="#">Organic</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><i class="fas fa-hamburger"></i></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">TRENDING</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Store</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Organic</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="px-2 search" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+<section class="main">
+    <div class="container py-5">
+        <div class="row py-5">
+            <div class="col-lg-7 pt-5 text-center">
+                <h1 class="pt-5">Nature Has Always Cared For Us !</h1>
+                <button class="btn1 mt-3">More Tips</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="new">
+    <div class="container py-5">
+        <div class="row pt-5">
+            <div class="col-lg-7 m-auto">
+                <div class="row text-center">
+                    <div class="col-lg-4">
+                        <img src="./img/grape.png" class="img-luid" alt="">
+                        <h6>NATURAL</h6>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="./img/banana.png" class="img-luid" alt="">
+                        <h6>NATURAL</h6>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="./img/tree.png" class="img-luid" alt="">
+                        <h6>NATURAL</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="product">
+    <div class="container py-5">
+        <div class="row py-5">
+            <div class="col-lg-5 m-auto text-center">
+                <h1>What's Trending</h1>
+                <h6 style="color: red;">Be Helaty Organic Food</h6>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/energy.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/real.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/drink.webp" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/healthy.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+        </div>
+       <div class="row">
+        <div class="col-lg-6 text-center m-auto">
+            <button class="btn1">Click For More</button>
+        </div>
+       </div>
+    </div>
+</section>
+
+<section class="about">
+    <div class="container py-5">
+        <div class="row py-5">
+            <div class="col-lg-8 m-auto text-center">
+                <h1>Welcom To Our Organic Food Sociaty</h1>
+                <h6 style="color: red;">Be Helaty Organic Food</h6>
+            </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <img src="./img/women5.jpg" class="img-fluid mb-3" alt="">
+                    <h5>Best Quality Product</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque error eius facilis architecto laboriosam quidem perferendis ab autem sequi iure.</p>
+                </div>
+                <div class="col-lg-4">
+                    <img src="./img/women4.jpg" class="img-fluid mb-3" alt="">
+                    <h5>Best Quality Product</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque error eius facilis architecto laboriosam quidem perferendis ab autem sequi iure.</p>
+                </div>
+                <div class="col-lg-4">
+                    <img src="./img/women3.avif" class="img-fluid mb-3" alt="">
+                    <h5>Best Quality Product</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque error eius facilis architecto laboriosam quidem perferendis ab autem sequi iure.</p>
+                </div>
+            </div>
+            <div class="row pt-3">
+                <div class="col-lg-6 text-center m-auto">
+                <button class="btn1">Shop More</button>
+                </div>
+            </div>
+    </div>
+</section>
+
+<section class="shop">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-8 m-auto text-center">
+                <h1>Explore Our Store</h1>
+                <h6 style="color: red;">Pick Your Product From Our Store</h6>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/energy.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/real.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/drink.webp" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/healthy.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/energy.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/real.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/drink.webp" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/healthy.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/energy.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/real.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/drink.webp" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="./img/healthy.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Energy Food</h6>
+                <p>$39.99</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="apple py-5">
+    <div class="container text-white py-5">
+        <div class="row py-5">
+            <div class="col-lg-6">
+                <h1 class="font-weight-bold py-3">Unlcok Your Potential With Good Nutrion</h1>
+                <h6>Be Healthy Organic Food</h6>
+                <button class="btn1 mt-3">More From Us</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="contact py-5">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-5 m-auto text-center">
+                <h1>Contact Us</h1>
+                <h6 style="color: red;">Always Be In Touch With Us</h6>
+            </div>
+        </div>
+        <div class="row py-5">
+            <div class="col-lg-9 m-auto">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h6>LOCATION</h6>
+                        <p>New York 0911 First Street DC</p>
+                    
+                        <h6>PHONE</h6>
+                        <p>03444-24359555</p>
+                    
+                        <h6>EMAIL</h6>
+                        <p>neki@novi.com</p>
+                    </div>
+
+                    <div class="col-lg-7">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <input type="text" class="form-control bg-light" placeholder="firstname">
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="text" class="form-control bg-light" placeholder="lastname">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 py-3">
+                                <textarea name="" class="form-control bg-light" placeholder="Enter Your Name" col="10" row="5"></textarea>
+                            </div>
+                        </div>
+                        <button class="btn1">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="news py-5">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-9 m-auto text-center">
+                <h1>Join Our Secret Sociaty</h1>
+                <input type="text" class="px-3" placeholder="Enter Your Email">
+                <button class="btn2">Submit</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-11">
+                <div class="row">
+                    <div class="col-lg-3 py-3">
+                        <h5 class="pb-3">CUSTOMER CARE</h5>
+                        <p>Regular</p>
+                        <p>On Time</p>
+                        <p>Always Care</p>
+                    </div>
+                    <div class="col-lg-3 py-3">
+                        <h5 class="pb-3">CUSTOMER CARE</h5>
+                        <p>Regular</p>
+                        <p>On Time</p>
+                        <p>Always Care</p>
+                    </div>
+                    <div class="col-lg-3 py-3">
+                        <h5 class="pb-3">CUSTOMER CARE</h5>
+                        <p>Regular</p>
+                        <p>On Time</p>
+                        <p>Always Care</p>
+                    </div>
+                    <div class="col-lg-3 py-3">
+                        <h5 class="pb-3">CUSTOMER CARE</h5>
+                        <span><i class="fab fa-facebook"></i></span>
+                        <span><i class="fab fa-instagram"></i></span>
+                        <span><i class="fab fa-twitter"></i></span>
+                        <span><i class="fab fa-google-plus"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <p class="text-center">Copyright 2021 All rights reserved | This temple is made by me!</p>
+    </div>
+</section>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+</body>
+</html>
